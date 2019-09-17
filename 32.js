ac4c0c863a720f940f9a9007cf55d42f94853e92
Julieta <juliiy98@gmail.com>
2019-09-16 20:15:36 -0600

    Archivo 32

---
+++ 32.js
@@ -0,0 +1,10 @@
+//Crear un arreglo de 20 elementos con nombres de personas.
+// Mostrar los elementos de la lista
+//debiendo ir cada uno en una fila distinta.
+var nombres = ["Luis", "Alfredo","Julieta","Gaby","Alberto","Rogelio",
+                "Maria","Jose","Luz"];
+
+for(var i=0;i<nombres.length;i++){
+
+    console.log(nombres[i]);
+}
