5f11559ef53abae84a21804954652b8aa509cf1b
Julieta <juliiy98@gmail.com>
2019-09-16 19:27:30 -0600

    Archivo 26

---
+++ 26.js
@@ -0,0 +1,11 @@
+//26.- Introducir una frase por teclado e imprimirla en el centro de la pantalla.
+
+var center = require('center-align');
+console.log(center([
+    'Lorem ipsum dolor sit amet,',
+    'consectetur adipiscing',
+    'elit, sed do eiusmod tempor incididunt',
+    'ut labore et dolore',
+    'magna aliqua. Ut enim ad minim',
+    'veniam, quis'
+  ]));
